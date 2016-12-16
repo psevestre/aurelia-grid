72f76bbcf1ff0b9c3368ca3e32c6bb92e8c33d4f
Philippe <phil@lighthouse.com.br>
2016-12-16 18:49:17 -0200

    Explain hack

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ import {GridLoader} from './grid';
 export function configure(aurelia,configCallback){
 
 	// Dummy call that forces the transpiler to require() grid.
-	// We need this or otherwhise our modules don´t get bundled when
-	// used by client apps (at least using aurelia-cli)
+	// Without this call our modules don´t get bundled when
+	// used by client apps (at least using aurelia-cli) as they are
+	// not traced 
 	let g : GridLoader  = new GridLoader();
 
 	// Register grid as a global resource so we don´t have
